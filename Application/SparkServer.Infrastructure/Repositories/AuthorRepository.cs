92f5aca8bf6590d3b1f7307968336f9f5dc2cfa3
Brandon Bruno <bmbruno@gmail.com>
2023-07-25 13:59:48 -0400

    Added Data/Infrastructure projects with author repo and Author stub class.

---
+++ Application/SparkServer.Infrastructure/Repositories/AuthorRepository.cs
@@ -0,0 +1,120 @@
+﻿using SparkServer.Core.Repositories;
+using SparkServer.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkServer.Infrastructure.Repositories
+{
+    public class AuthorRepository : IAuthorRepository<Author>
+    {
+        public Author Get(int ID)
+        {
+            //Author item;
+
+            //using (var db = new SparkServerEntities())
+            //{
+            //    item = db.Author.FirstOrDefault(u => u.ID == ID);
+            //}
+
+            //return item;
+
+            return new Author();
+        }
+
+        public Author Get(Guid ssoID)
+        {
+            //Author item;
+
+            //using (var db = new SparkServerEntities())
+            //{
+            //    item = db.Author.FirstOrDefault(u => u.SSOID == ssoID);
+            //}
+
+            //return item;
+
+            return new Author();
+        }
+
+        public IEnumerable<Author> Get(Expression<Func<Author, bool>> whereClause)
+        {
+            // CALLING: ArticleRepo.Get(x => x.Title == "abcdef");
+            // USING: db.Articles.Where(whereClause);
+
+            //List<Author> results;
+
+            //using (var db = new SparkServerEntities())
+            //{
+            //    results = db.Author.Where(whereClause).ToList();
+            //}
+
+            //return results;
+
+            return new List<Author>();
+        }
+
+        public IEnumerable<Author> GetAll()
+        {
+            //List<Author> results;
+
+            //using (var db = new SparkServerEntities())
+            //{
+            //    results = db.Author.ToList();
+            //}
+
+            //return results;
+
+            return new List<Author>();
+        }
+
+        public int Create(Author newItem)
+        {
+            //using (var db = new SparkServerEntities())
+            //{
+            //    db.Author.Add(newItem);
+            //    db.SaveChanges();
+            //}
+
+            //return newItem.ID;
+
+            return 0;
+        }
+
+        public void Update(Author updateItem)
+        {
+            //using (var db = new SparkServerEntities())
+            //{
+            //    Author toUpdate = db.Author.FirstOrDefault(u => u.ID == updateItem.ID);
+
+            //    if (toUpdate == null)
+            //        throw new Exception($"Could not find Author with ID of {updateItem.ID}");
+
+            //    toUpdate = updateItem;
+            //    db.Entry(toUpdate).State = System.Data.Entity.EntityState.Modified;
+            //    db.SaveChanges();
+            //}
+
+            return;
+        }
+
+        public void Delete(int ID)
+        {
+            //using (var db = new SparkServerEntities())
+            //{
+            //    Author toDelete = db.Author.FirstOrDefault(u => u.ID == ID);
+
+            //    if (toDelete == null)
+            //        throw new Exception($"Could not find Author with ID of {ID}");
+
+            //    toDelete.Active = false;
+
+            //    db.SaveChanges();
+            //}
+
+            return;
+        }
+    }
+}
