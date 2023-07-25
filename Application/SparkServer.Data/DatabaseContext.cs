6938e87849e946300b8d4ae67646016efbb736a6
Brandon Bruno <bmbruno@gmail.com>
2023-07-25 15:35:07 -0400

    Added DBContext items.

---
+++ Application/SparkServer.Data/DatabaseContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using SparkServer.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkServer.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Author> Authors { private get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=SparkServer.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Author>().ToTable("Authors");
+        }
+    }
+}
