65f0e31167981ba9cf7c59324184121c98ba56b6
Brandon Bruno <bmbruno@gmail.com>
2023-07-25 14:19:07 -0400

    Added Blog data model. Stubbed Blog repo class.

--- Application/SparkServer.Infrastructure/Repositories/BlogRepository.cs
+++ Application/SparkServer.Infrastructure/Repositories/BlogRepository.cs
@@ -1,12 +1,196 @@
-﻿using System;
+﻿using SparkServer.Core.Repositories;
+using SparkServer.Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SparkServer.Infrastructure.Repositories
 {
-    internal class BlogRepository
+    public class BlogRepository : IBlogRepository<Blog>
     {
+        public Blog Get(int ID)
+        {
+            //Blog item;
+
+            //using (var db = new SparkServerEntities())
+            //{
+            //    item = db.Blog.Include(u => u.Author).Include(u => u.BlogsTags).FirstOrDefault(u => u.ID == ID);
+            //}
+
+            //return item;
+
+            return new Blog();
+        }
+
+        public IEnumerable<Blog> Get(Expression<Func<Blog, bool>> whereClause)
+        {
+            // CALLING: ArticleRepo.Get(x => x.Title == "abcdef");
+            // USING: db.Articles.Where(whereClause);
+
+            //List<Blog> results;
+
+            //using (var db = new SparkServerEntities())
+            //{
+            //    results = db.Blog.Where(whereClause).Include(u => u.Author).Include(u => u.BlogsTags).ToList();
+            //}
+
+            //return results;
+
+            return new List<Blog>();
+        }
+
+        public IEnumerable<Blog> GetAll()
+        {
+            //List<Blog> results;
+
+            //using (var db = new SparkServerEntities())
+            //{
+            //    results = db.Blog.Where(u => u.Active).Include(u => u.Author).Include(u => u.BlogsTags).ToList();
+            //}
+
+            //return results;
+
+            return new List<Blog>();
+        }
+
+        public int Create(Blog newItem)
+        {
+            //using (var db = new SparkServerEntities())
+            //{
+            //    db.Blog.Add(newItem);
+            //    db.SaveChanges();
+            //}
+
+            //return newItem.ID;
+
+            return 0;
+        }
+
+        public void Update(Blog updateItem)
+        {
+            //using (var db = new SparkServerEntities())
+            //{
+            //    db.Blog.Attach(updateItem);
+
+            //    var entry = db.Entry(updateItem);
+            //    entry.Property(e => e.Title).IsModified = true;
+            //    entry.Property(e => e.Subtitle).IsModified = true;
+            //    entry.Property(e => e.Body).IsModified = true;
+            //    entry.Property(e => e.PublishDate).IsModified = true;
+            //    entry.Property(e => e.AuthorID).IsModified = true;
+            //    entry.Property(e => e.UniqueURL).IsModified = true;
+            //    entry.Property(e => e.ImagePath).IsModified = true;
+            //    entry.Property(e => e.ImageThumbnailPath).IsModified = true;
+
+            //    db.SaveChanges();
+            //}
+
+            return;
+        }
+
+        public void Delete(int ID)
+        {
+            //using (var db = new SparkServerEntities())
+            //{
+            //    Blog toDelete = db.Blog.FirstOrDefault(u => u.ID == ID);
+
+            //    if (toDelete == null)
+            //        throw new Exception($"Could not find Blog with ID of {ID}");
+            //    toDelete.Active = false;
+
+            //    db.SaveChanges();
+            //}
+
+            return;
+        }
+
+        public Blog Get(int year, int month, string uniqueURL)
+        {
+            //// TODO: This probably needs to be finished (year, month are not being used!)
+
+            //Blog item = null;
+
+            //using (var db = new SparkServerEntities())
+            //{
+            //    item = db.Blog.FirstOrDefault(u => u.UniqueURL == uniqueURL);
+
+            //    db.Entry(item).Reference(la => la.Author).Load();
+            //    db.Entry(item).Collection(la => la.BlogsTags).Load();
+            //}
+
+            //return item;
+
+            return new Blog();
+        }
+
+        public IEnumerable<Blog> GetByDate(int year, int? month)
+        {
+            //List<Blog> blogList = new List<Blog>();
+
+            //using (var db = new SparkServerEntities())
+            //{
+            //    if (month.HasValue)
+            //    {
+            //        blogList = db.Blog.Where(
+            //            u => u.PublishDate <= DateTime.Now &&
+            //            u.Active &&
+            //            u.PublishDate.Value.Year == year &&
+            //            u.PublishDate.Value.Month == month).Include(u => u.Author).Include(u => u.BlogsTags).ToList();
+            //    }
+            //    else
+            //    {
+            //        blogList = db.Blog.Where(
+            //            u => u.PublishDate <= DateTime.Now && u.Active && u.PublishDate.Value.Year == year).Include(u => u.Author).Include(u => u.BlogsTags).ToList();
+            //    }
+            //}
+
+            //return blogList;
+
+            return new List<Blog>();
+        }
+
+        public IEnumerable<Blog> GetRecent(int numberToLoad)
+        {
+            //List<Blog> blogList = new List<Blog>();
+
+            //using (var db = new SparkServerEntities())
+            //{
+            //    blogList = db.Blog.Where(u => u.Active)
+            //                      .Where(u => u.PublishDate <= DateTime.Now)
+            //                      .Include(a => a.Author)
+            //                      .Include(a => a.BlogsTags)
+            //                      .OrderByDescending(u => u.PublishDate)
+            //                      .Take(numberToLoad)
+            //    .ToList();
+            //}
+
+            //return blogList;
+
+            return new List<Blog>();
+        }
+
+        public IEnumerable<Blog> GetByTagID(int tagID)
+        {
+            //List<Blog> blogList = new List<Blog>();
+
+            //using (var db = new SparkServerEntities())
+            //{
+            //    blogList = db.BlogsTags.Where(u => u.TagID == tagID)
+            //                           .Select(p => p.Blog)
+            //                           .Where(p => p.PublishDate <= DateTime.Now)
+            //                           .Where(p => p.Active)
+            //                           .Include(a => a.Author)
+            //                           .Include(a => a.BlogsTags)
+            //                           .ToList();
+            //}
+
+            //return blogList;
+
+            return new List<Blog>();
+        }
     }
 }
