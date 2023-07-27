073a18b1abd90c12a9795eef1376ee8d221eff96
Brandon Bruno <bmbruno@gmail.com>
2023-07-27 12:39:14 -0400

    Added test Blog data to SQL script. Index stub tweaks.

--- SQLScripts/01_InsertTestData.sql
+++ SQLScripts/01_InsertTestData.sql
@@ -21,6 +21,12 @@ FROM Authors;
 INSERT INTO Blogs (Title, Subtitle, Content, Slug, PublishDate, AuthorID)
 VALUES ('Test Title', 'Subtitle!!!', '<p>Hello, world!</p>', 'test-blog-alpha', '2023-07-25', 1);
 
+INSERT INTO Blogs (Title, Subtitle, Content, Slug, PublishDate, AuthorID)
+VALUES ('What a Great Blog Post', 'This is my subtitle.', '<p>Lorem ipsum.</p>', 'test-blog-bravo', '2023-06-30', 1);
+
+INSERT INTO Blogs (Title, Subtitle, Content, Slug, PublishDate, AuthorID)
+VALUES ('How Much Front-End Do I Need to Know?', 'It''s time to embrace JavaScript.', '<p>Sitecore XM Cloud is Sitecore''s first fully SaaS ("software as a service") product built from the ground-up and will likely be the company''s flagship CMS product in the coming year.</p>', 'test-blog-charlie', '2023-03-13', 1);
+
 SELECT *
 FROM Blogs;
 
