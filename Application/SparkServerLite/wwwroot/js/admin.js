1a9da73346d080dbf3907b8cc461f830ca53f7ff
Brandon Bruno <bmbruno@gmail.com>
2023-08-24 08:41:20 -0400

    Initial implementation of JS logic for loading blog media.

--- Application/SparkServerLite/wwwroot/js/admin.js
+++ Application/SparkServerLite/wwwroot/js/admin.js
@@ -1,7 +1,36 @@
-﻿// SparkServerLite Admin scripts
+﻿(function () {
 
-(function () {
+    window.SparkServerAdmin = window.SparkServerAdmin || {
 
-    
+        settings: {
+
+            siteURL: ""
+
+        },
+
+        endpoints: {
+
+            blogMedia: "/api/blogmedia"
+
+        },
+
+        loadBlogMediaList: async function () {
+
+            // Get BlogID from page
+            let blogID = document.getElementById("ID");
+
+            // Call API endpoint to get media items
+            let response = await fetch(SparkServerAdmin.endpoints.blogMedia);
+            let data = await response.json();
+
+            // Build unordered list from data
+
+        }
+
+
+    };
+
+
+
 
 })();
