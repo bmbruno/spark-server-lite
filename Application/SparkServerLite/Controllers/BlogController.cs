a8e4e97698974a5ced9eb9b5406a8d1f7c2b2b2e
Brandon Bruno <bmbruno@gmail.com>
2023-10-02 13:27:48 -0400

    Added error handling to public blog controller actions. Re-centered 'container' content.

--- Application/SparkServerLite/Controllers/BlogController.cs
+++ Application/SparkServerLite/Controllers/BlogController.cs
@@ -34,35 +34,43 @@ namespace SparkServerLite.Controllers
 
             base.Setup(viewModel, _settings);
 
-            if (year.HasValue && month.HasValue)
+            try
             {
-                // Blogs list by year + month
-                blogList = _blogRepo.GetByDate(year.Value, month.Value).ToList();
-                string monthName = new DateTime(year.Value, month.Value, 1).ToString("MMMM");
-                viewModel.Header = $"Blog Posts for {monthName} {year.ToString()}";
+                if (year.HasValue && month.HasValue)
+                {
+                    // Blogs list by year + month
+                    blogList = _blogRepo.GetByDate(year.Value, month.Value).ToList();
+                    string monthName = new DateTime(year.Value, month.Value, 1).ToString("MMMM");
+                    viewModel.Header = $"Blog Posts for {monthName} {year.ToString()}";
+                }
+                else if (year.HasValue)
+                {
+                    // Blog list by year only
+                    blogList = _blogRepo.GetByDate(year.Value, null).ToList();
+                    viewModel.Header = $"Blog Posts for {year.ToString()}";
+                }
+                else
+                {
+                    // Default: blog overview (top posts)
+                    blogList = _blogRepo.GetAllPublished().ToList();
+                    viewModel.Header = "Latest Blog Posts";
+                }
+
+                tagList = _blogTagRepo.GetTagsInUse().ToList();
+
+                // Paging
+                int totalCount = blogList.Count;
+                blogList = blogList.Skip(this.SkipCount).Take(this.ItemsPerPage).ToList();
+                viewModel.MapToViewModel(blogList, tagList, _settings);
+
+                viewModel.Paging.PageCount = (totalCount + (this.ItemsPerPage - 1)) / this.ItemsPerPage;
+                viewModel.Paging.CurrentPage = this.Page;
             }
-            else if (year.HasValue)
+            catch (Exception exc)
             {
-                // Blog list by year only
-                blogList = _blogRepo.GetByDate(year.Value, null).ToList();
-                viewModel.Header = $"Blog Posts for {year.ToString()}";
+                // TODO: log this exception
+                TempData["Error"] = $"Error loading blog posts. Exception: {exc.Message}";
             }
-            else
-            {
-                // Default: blog overview (top posts)
-                blogList = _blogRepo.GetAllPublished().ToList();
-                viewModel.Header = "Latest Blog Posts";
-            }
-
-            tagList = _blogTagRepo.GetTagsInUse().ToList();
-
-            // Paging
-            int totalCount = blogList.Count;
-            blogList = blogList.Skip(this.SkipCount).Take(this.ItemsPerPage).ToList();
-            viewModel.MapToViewModel(blogList, tagList, _settings);
-
-            viewModel.Paging.PageCount = (totalCount + (this.ItemsPerPage - 1)) / this.ItemsPerPage;
-            viewModel.Paging.CurrentPage = this.Page;
 
             return View(viewModel);
         }
@@ -76,32 +84,43 @@ namespace SparkServerLite.Controllers
             if (String.IsNullOrEmpty(slug))
                 return RedirectToAction(actionName: "Index", controllerName: "Blog");
 
-            Blog blog = _blogRepo.Get(slug);
-            IEnumerable<BlogTag> blogTags = _blogTagRepo.GetForBlog(blog.ID);
-
-            viewModel.MapToViewModel(blog, blogTags, _settings);
-            viewModel.IsPreview = preview;
-            viewModel.MenuSelection = Infrastructure.Enums.MainMenu.Blog;
-
-            // Should this blog post be displayed at all? (Preview flag overrides denied access in some cases)
-            bool shouldDisplay = false;
-
-            // Has published date passed?
-            if (viewModel.PublishDate <= DateTime.Now)
+            try
             {
-                shouldDisplay = true;
+                Blog blog = _blogRepo.Get(slug);
+                IEnumerable<BlogTag> blogTags = _blogTagRepo.GetForBlog(blog.ID);
+
+                viewModel.MapToViewModel(blog, blogTags, _settings);
+                viewModel.IsPreview = preview;
+                viewModel.MenuSelection = Infrastructure.Enums.MainMenu.Blog;
+
+                // Should this blog post be displayed at all? (Preview flag overrides denied access in some cases)
+                bool shouldDisplay = false;
+
+                // Has published date passed?
+                if (viewModel.PublishDate <= DateTime.Now)
+                {
+                    shouldDisplay = true;
+                }
+
+                // Is preview mode being used with an authenticated user?
+                if (preview && User != null && User.Identity.IsAuthenticated)
+                {
+                    shouldDisplay = true;
+                }
+
+                if (shouldDisplay)
+                    return View(viewModel);
+                else
+                    return RedirectToAction(actionName: "Index", controllerName: "Home");
+
+            }
+            catch (Exception exc)
+            {
+                // TODO: log this exception
+                TempData["Error"] = $"Error loading blog post. Exception: {exc.Message}";
             }
 
-            // Is preview mode being used with an authenticated user?
-            if (preview && User != null && User.Identity.IsAuthenticated)
-            {
-                shouldDisplay = true;
-            }
-
-            if (shouldDisplay)
-                return View(viewModel);
-            else
-                return RedirectToAction(actionName: "Index", controllerName: "Home");
+            return View(viewModel);
         }
 
         [HttpGet("posts/tag/{tagName}")]
@@ -115,19 +134,27 @@ namespace SparkServerLite.Controllers
 
             base.Setup(viewModel, _settings);
 
-            string unencodedTagName = FormatHelper.GetTagNameFromURL(tagName);
-            blogList = _blogRepo.GetByTagName(unencodedTagName).ToList();
-            tagList = _blogTagRepo.GetTagsInUse().ToList();
+            try
+            {
+                string unencodedTagName = FormatHelper.GetTagNameFromURL(tagName);
+                blogList = _blogRepo.GetByTagName(unencodedTagName).ToList();
+                tagList = _blogTagRepo.GetTagsInUse().ToList();
 
-            viewModel.MapToViewModel(blogList, tagList, _settings);
-            viewModel.Header = $"Posts tagged '{unencodedTagName}'";
+                viewModel.MapToViewModel(blogList, tagList, _settings);
+                viewModel.Header = $"Posts tagged '{unencodedTagName}'";
 
-            // Paging
-            int totalItems = blogList.Count;
-            viewModel.BlogList = viewModel.BlogList.OrderByDescending(u => u.PublishDate).Skip(this.SkipCount).Take(this.ItemsPerPage).ToList();
+                // Paging
+                int totalItems = blogList.Count;
+                viewModel.BlogList = viewModel.BlogList.OrderByDescending(u => u.PublishDate).Skip(this.SkipCount).Take(this.ItemsPerPage).ToList();
 
-            viewModel.Paging.PageCount = (totalItems + this.ItemsPerPage - 1) / this.ItemsPerPage;
-            viewModel.Paging.CurrentPage = this.Page;
+                viewModel.Paging.PageCount = (totalItems + this.ItemsPerPage - 1) / this.ItemsPerPage;
+                viewModel.Paging.CurrentPage = this.Page;
+            }
+            catch (Exception exc)
+            {
+                // TODO: log this exception
+                TempData["Error"] = $"Error loading blogs by tag. Exception: {exc.Message}";
+            }
 
             return View(viewName: "Index", model: viewModel);
         }
