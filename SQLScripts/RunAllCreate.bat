de831930852d78b115ec3c869e11f1279cd3ffe0
Brandon Bruno <bmbruno@gmail.com>
2025-06-18 19:50:01 -0400

    Added Analytics database file creation to SQL setup scripts.

--- SQLScripts/RunAllCreate.bat
+++ SQLScripts/RunAllCreate.bat
@@ -1,3 +1,4 @@
 sqlite3.exe ..\Application\SparkServerLite\SparkServer.db < 10_CreateAll.sql
-
 sqlite3.exe ..\Application\SparkServerLite\SparkServer.db < 10_InsertTestData.sql
+
+sqlite3.exe ..\Application\SparkServerLite\SparkServerAnalytics.db < 20_CreateAnalytics.sql
