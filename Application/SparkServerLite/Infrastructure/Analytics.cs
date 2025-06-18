fba647ba50783320e9a9d17c1097f112a538e5f2
Brandon Bruno <bmbruno@gmail.com>
2025-06-18 16:36:29 -0400

    Some RecordVisit logic.

--- Application/SparkServerLite/Infrastructure/Analytics.cs
+++ Application/SparkServerLite/Infrastructure/Analytics.cs
@@ -13,9 +13,29 @@ namespace SparkServerLite.Infrastructure
             _settings = settings;
         }
 
-        public void RecordVisit(string page, string userAgent, string referer)
+        /// <summary>
+        /// Records a single visit to the database.
+        /// </summary>
+        /// <param name="page">Page URL of this visit.</param>
+        /// <param name="userAgent">User Agent string, if available.</param>
+        /// <param name="referer"></param>
+        public void RecordVisit(string page, string userAgent = "", string referer = "")
         {
+            if (String.IsNullOrEmpty(page))
+            {
+                // TODO: log 'page empty' condition
+                return;
+            }
+
+            Visit visit = new Visit();
+            visit.Date = DateTime.Now;
+            visit.Page = page.Trim();
+
             // TODO: extract useragent into variables
+            if (!String.IsNullOrEmpty(userAgent))
+            {
+                visit.UserAgent.Trim();
+            }
 
             // TODO: store in database
 
