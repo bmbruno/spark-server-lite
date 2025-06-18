920dfc09b4045b065e4556f424054da618dca63f
Brandon Bruno <bmbruno@gmail.com>
2025-06-18 15:35:40 -0400

    Added fields to Visit model.

--- Application/SparkServerLite/Models/Visit.cs
+++ Application/SparkServerLite/Models/Visit.cs
@@ -2,6 +2,24 @@
 {
     public class Visit
     {
-        
+        public int ID { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string UserAgent { get; set; } = string.Empty;
+
+        public string Domain { get; set; } = string.Empty;
+
+        public string Page { get; set; } = string.Empty;
+
+        public string Referer { get; set; } = string.Empty;
+
+        public string OS { get; set; } = string.Empty;
+
+        public string Device { get; set; } = string.Empty;
+
+        public bool Active { get; set; }
+
+        public DateTime CreateDate { get; set; }
     }
 }
