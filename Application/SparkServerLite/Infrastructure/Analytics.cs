324b4852255e90d9c75febb7e06901f38ce80db5
Brandon Bruno <bmbruno@gmail.com>
2025-06-19 12:02:27 -0400

    Added additional Visit creation logic.

--- Application/SparkServerLite/Infrastructure/Analytics.cs
+++ Application/SparkServerLite/Infrastructure/Analytics.cs
@@ -1,16 +1,19 @@
 ﻿using Microsoft.Data.Sqlite;
 using SparkServerLite.Interfaces;
 using SparkServerLite.Models;
+using SparkServerLite.Repositories;
 
 namespace SparkServerLite.Infrastructure
 {
     public class Analytics
     {
         private readonly IAppSettings _settings;
+        private readonly IAnalyticsRepository<Visit> _analyticsRepo;
 
-        public Analytics(AppSettings settings)
+        public Analytics(IAppSettings settings, IAnalyticsRepository<Visit> repo)
         {
             _settings = settings;
+            _analyticsRepo = repo;
         }
 
         /// <summary>
@@ -18,18 +21,21 @@ namespace SparkServerLite.Infrastructure
         /// </summary>
         /// <param name="page">Page URL of this visit.</param>
         /// <param name="userAgent">User Agent string, if available.</param>
-        /// <param name="referer"></param>
+        /// <param name="referer">URL/page referer from the request.</param>
         public void RecordVisit(string page, string userAgent = "", string referer = "")
         {
             if (String.IsNullOrEmpty(page))
             {
-                // TODO: log 'page empty' condition
+                // TODO: log 'page empty' condition - this should not really happen since page is handled by the app, not the user's request
                 return;
             }
 
             Visit visit = new Visit();
-            visit.Date = DateTime.Now;
+            visit.Active = true;
+            visit.CreateDate = DateTime.Now;
+            visit.Date = visit.CreateDate;
             visit.Page = page.Trim();
+            visit.Referer = !String.IsNullOrEmpty(referer) ? referer.Trim() : null;
 
             // TODO: extract useragent into variables
             if (!String.IsNullOrEmpty(userAgent))
@@ -39,7 +45,14 @@ namespace SparkServerLite.Infrastructure
 
             // TODO: store in database
 
-            // TODO: handle concurrency issues/errors
+            try
+            {
+                _analyticsRepo.Create(visit);
+            }
+            catch (Exception exc)
+            {
+                // TODO: handle concurrency issues/errors
+            }
         }
     }
 }
