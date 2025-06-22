c5f5223a24233c3efb2fb6bbaef982fc02c95653
Brandon Bruno <bmbruno@gmail.com>
2025-06-22 11:54:18 -0400

    Inline comments for IAnalyticsRepository.

--- Application/SparkServerLite/Interfaces/IAnalyticsRepository.cs
+++ Application/SparkServerLite/Interfaces/IAnalyticsRepository.cs
@@ -2,10 +2,23 @@
 {
     public interface IAnalyticsRepository<T>
     {
+        /// <summary>
+        /// Gets an individual Visit from the database for the given ID.
+        /// </summary>
+        /// <param name="ID">ID of the Visit.</param>
+        /// <returns>Visit object.</returns>
         public T Get(int ID);
 
+        /// <summary>
+        /// Gets a list of all Visits from the database. Should probably use this with caution, as there may be lots of records.
+        /// </summary>
+        /// <returns>Enumerable list of all Visits.</returns>
         public IEnumerable<T> GetAll();
         
+        /// <summary>
+        /// Inserts a Visit record into the database.
+        /// </summary>
+        /// <param name="newItem">Visit object.</param>
         void Create(T newItem);
     }
 }
