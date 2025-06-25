7cde2d64ffc746e741c8910794078a94e5de88bd
Brandon Bruno <bmbruno@gmail.com>
2025-06-25 08:25:21 -0400

    Switched to DBNull values for Create nulls.

--- Application/SparkServerLite/Repositories/AnalyticsRepository.cs
+++ Application/SparkServerLite/Repositories/AnalyticsRepository.cs
@@ -33,15 +33,15 @@ namespace SparkServerLite.Repositories
                 
                 command.CommandText = @"INSERT INTO Visits (Date, UserAgent, Domain, Page, Referer, OS, Device, Active, CreateDate) VALUES ($date, $useragent, $domain, $page, $referer, $os, $device, $active, $createdate);";
                 command.Parameters.AddWithValue("$date", newItem.Date);
-                command.Parameters.AddWithValue("$useragent", !String.IsNullOrEmpty(newItem.UserAgent) ? newItem.UserAgent : null);
-                command.Parameters.AddWithValue("$domain", !String.IsNullOrEmpty(newItem.Domain) ? newItem.Domain : null);
-                command.Parameters.AddWithValue("$page", !String.IsNullOrEmpty(newItem.Page) ? newItem.Page : null);
-                command.Parameters.AddWithValue("$referer", !String.IsNullOrEmpty(newItem.Referer) ? newItem.Referer : null);
-                command.Parameters.AddWithValue("$os", !String.IsNullOrEmpty(newItem.OS) ? newItem.OS : null);
-                command.Parameters.AddWithValue("$device", !String.IsNullOrEmpty(newItem.Device) ? newItem.Device : null);
+                command.Parameters.AddWithValue("$useragent", !String.IsNullOrEmpty(newItem.UserAgent) ? newItem.UserAgent : DBNull.Value);
+                command.Parameters.AddWithValue("$domain", !String.IsNullOrEmpty(newItem.Domain) ? newItem.Domain : DBNull.Value);
+                command.Parameters.AddWithValue("$page", !String.IsNullOrEmpty(newItem.Page) ? newItem.Page : DBNull.Value);
+                command.Parameters.AddWithValue("$referer", !String.IsNullOrEmpty(newItem.Referer) ? newItem.Referer : DBNull.Value);
+                command.Parameters.AddWithValue("$os", !String.IsNullOrEmpty(newItem.OS) ? newItem.OS : DBNull.Value);
+                command.Parameters.AddWithValue("$device", !String.IsNullOrEmpty(newItem.Device) ? newItem.Device : DBNull.Value);
                 
                 command.Parameters.AddWithValue("$active", newItem.Active);
-                command.Parameters.AddWithValue("$createDate", newItem.CreateDate.ToString(FormatHelper.SQLiteDateTime));
+                command.Parameters.AddWithValue("$createdate", newItem.CreateDate.ToString(FormatHelper.SQLiteDateTime));
                 
                 conn.Open();
                 command.ExecuteNonQuery();
