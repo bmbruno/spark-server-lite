58a352eae4df6e776eb03eb6e02ae2635d0495b0
Brandon Bruno <bmbruno@gmail.com>
2026-09-23 10:58:00 -0400

    Stubbed out SEO admin section.

---
+++ Application/SparkServerLite/Controllers/SeoController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SparkServerLite.Infrastructure;
+using SparkServerLite.Interfaces;
+using SparkServerLite.Models.Analytics;
+using SparkServerLite.ViewModels;
+using SparkServerLite.ViewModels.Analytics;
+
+namespace SparkServerLite.Controllers
+{
+    [Authorize]
+    public class SeoController : BaseController
+    {
+        private readonly IWebHostEnvironment _host;
+                
+        public SeoController(Interfaces.ILogger logger, IWebHostEnvironment host, IAppSettings settings, IAppContent content) : base(settings, content, logger)
+        {
+            _host = host;
+        }
+
+        public IActionResult Index()
+        {
+            BaseViewModel viewModel = new();
+            base.Setup(viewModel);
+            ViewData["Title"] = "Analytics";
+
+            return View(viewModel);
+        }
+    }
+}
